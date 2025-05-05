ecf11c608950518df1ff9461a6366d534b512b77
Lucas Lima <lucasgabrieljake@gmail.com>
2025-05-05 12:05:15 -0300

    feat: docker in the application

--- Contexts/APIDbContextRecipe.cs
+++ Contexts/APIDbContextRecipe.cs
@@ -7,7 +7,7 @@ public class APIDbContextFactory : IDesignTimeDbContextFactory<APIDbContext>
     public APIDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<APIDbContext>();
-        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=recipes;Username=postgres;Password=postgres");
+        optionsBuilder.UseNpgsql("Host=database;Port=5432;Database=recipes;Username=postgres;Password=33647553");
 
         return new APIDbContext(optionsBuilder.Options);
     }
